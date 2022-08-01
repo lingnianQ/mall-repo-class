8f88383f0075a4fbd0f9e49596f5d9aaf5f1bed1
TEDU <83123095@qq.com>
2022-08-01 09:44:20 +0800

    初始化

--- doc/sql/mall_ams.sql
+++ doc/sql/mall_ams.sql
@@ -1,18 +1,5 @@
-/*
-SQLyog Community v13.1.6 (64 bit)
-MySQL - 5.7.33-log : Database - mall_ams
-*********************************************************************
-*/
 
-/*!40101 SET NAMES utf8 */;
-
-/*!40101 SET SQL_MODE=''*/;
-
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`mall_ams` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+CREATE DATABASE  `mall_ams`  DEFAULT CHARACTER SET utf8mb4;
 USE `mall_ams`;
 /*Table structure for table `ams_admin` */
 
@@ -174,7 +161,3 @@ insert  into `ams_role_permission`(`id`,`role_id`,`permission_id`,`gmt_create`,`
 (14,3,2,'2021-12-02 17:59:27','2021-12-02 17:59:27'),
 (15,3,3,'2021-12-02 17:59:27','2021-12-02 17:59:27');
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
