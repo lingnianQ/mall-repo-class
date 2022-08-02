ebdd43ca8c085bbcccccbafb3d83e80330caebd6
TEDU <83123095@qq.com>
2022-08-02 15:05:21 +0800

    spuDetailController代码

--- mall-front/mall-front-webapi/src/main/java/cn/tedu/mall/front/controller/FrontSpuController.java
+++ mall-front/mall-front-webapi/src/main/java/cn/tedu/mall/front/controller/FrontSpuController.java
@@ -11,6 +11,7 @@ import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
