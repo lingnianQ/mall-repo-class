b34eeeae990cf226df13e7b9bef7d0088178b8a1
TEDU <83123095@qq.com>
2022-08-05 12:00:48 +0800

    查询订单功能完成

--- mall-search/mall-search-webapi/src/main/java/cn/tedu/mall/search/repository/SpuForElasticRepository.java
+++ mall-search/mall-search-webapi/src/main/java/cn/tedu/mall/search/repository/SpuForElasticRepository.java
@@ -2,9 +2,11 @@ package cn.tedu.mall.search.repository;
 
 import cn.tedu.mall.pojo.search.entity.SpuForElastic;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
 
 // SpuForElastic实体类操作Es的持久层方法
 // 继承父接口,当前接口实现类具备基本增删改查功能
+@Repository
 public interface SpuForElasticRepository extends
                                 ElasticsearchRepository<SpuForElastic,Long> {
 
