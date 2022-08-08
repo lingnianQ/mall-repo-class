c5f39121312b560d41c771e1f6b05fd02141f912
TEDU <83123095@qq.com>
2022-08-08 16:59:58 +0800

    搜索功能的业务逻辑层

--- mall-seckill/mall-seckill-webapi/src/main/java/cn/tedu/mall/seckill/mapper/SeckillSpuMapper.java
+++ mall-seckill/mall-seckill-webapi/src/main/java/cn/tedu/mall/seckill/mapper/SeckillSpuMapper.java
@@ -3,6 +3,7 @@ package cn.tedu.mall.seckill.mapper;
 import cn.tedu.mall.pojo.seckill.model.SeckillSpu;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
@@ -10,5 +11,10 @@ public interface SeckillSpuMapper {
 
     // 查询秒杀商品列表
     List<SeckillSpu> findSeckillSpus();
+
+    // 根据给定的时间,查询秒杀商品信息
+    List<SeckillSpu> findSeckillSpusByTime(LocalDateTime time);
+
+
 
 }
