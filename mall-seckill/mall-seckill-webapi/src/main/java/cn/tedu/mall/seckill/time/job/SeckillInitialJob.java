baedd5dbd8afa11588c11f7d94f382b7bd99123f
TEDU <83123095@qq.com>
2022-08-09 10:47:27 +0800

    秒杀预热

--- mall-seckill/mall-seckill-webapi/src/main/java/cn/tedu/mall/seckill/time/job/SeckillInitialJob.java
+++ mall-seckill/mall-seckill-webapi/src/main/java/cn/tedu/mall/seckill/time/job/SeckillInitialJob.java
@@ -6,6 +6,7 @@ import cn.tedu.mall.seckill.mapper.SeckillSkuMapper;
 import cn.tedu.mall.seckill.mapper.SeckillSpuMapper;
 import cn.tedu.mall.seckill.utils.SeckillCacheUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.math.RandomUtils;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
@@ -15,6 +16,7 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class SeckillInitialJob implements Job {
@@ -56,8 +58,39 @@ public class SeckillInitialJob implements Job {
                 // 最终的常量可能为
                 // skuStockKey="mall:seckill:sku:stock:1"
                 String skuStockKey= SeckillCacheUtils.getStockKey(sku.getSkuId());
-
-
+                // 检查Redis中是否已经包含了这个key
+                if(redisTemplate.hasKey(skuStockKey)){
+                    // 如果Key已经存在,证明之前已经缓存过
+                    log.info("{}号商品已经在缓存中",sku.getSkuId());
+                }else{
+                    // 如果key不在Redis中,那么久需要将当前sku的库存
+                    // 利用StringRedisTemplate保存到Redis,方便操作库存减少
+                    // 缓存设置的过期时间,应该是秒杀活动的时间+5分钟
+                    stringRedisTemplate.boundValueOps(skuStockKey)
+                            .set(sku.getSeckillStock()+"",
+                                    125*60+ RandomUtils.nextInt(100), TimeUnit.SECONDS
+                                    );
+                    log.info("成功为{}号商品添加库存",sku.getSkuId());
+                }
+            }
+            // 上面给每个spu的sku列表商品赋值了库存数
+            // 下面要给当前spu赋值随机码
+            // 随机码的作用:
+            // 要想访问商品必须通过控制器url访问
+            // 例如:/seckill/spu/{id}  如果{id}的位置使用真实的id值
+            // 那么秒杀开始前就已经将访问商品的路径暴露了
+            // 可能会有投机者利用id访问商品信息,为了防止这样投机事情的发生,减少服务器压力
+            // 可以将{id}的位置生成为一个随机的数字,如果用户猜这个数字,猜中的几率会低很多,
+            // 秒杀开始5分钟前才生成随机码,有了随机码才可能访问到这个商品
+            // 获得随机码的Key
+            String randomCodeKey=SeckillCacheUtils.getRandCodeKey(spu.getSpuId());
+            // 判断随机码是否已经生成过
+            if(!redisTemplate.hasKey(randomCodeKey)){
+                // 如果随机码key为空,生成随机码
+                int randomCode=RandomUtils.nextInt(900000)+100000;
+                redisTemplate.boundValueOps(randomCodeKey)
+                        .set(randomCode,125*60+RandomUtils.nextInt(100),TimeUnit.SECONDS);
+                log.info("spuId为{}的商品随机码为{}",spu.getSpuId(),randomCode);
             }
         }
 
