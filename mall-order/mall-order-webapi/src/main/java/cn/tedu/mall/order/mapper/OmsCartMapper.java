e4657d0d70b94941aff7b8bbaa0ac32610f3cb74
liucangsong <liucangsong@gmail.com>
2022-08-30 17:31:33 +0800

    初始化

---
+++ mall-order/mall-order-webapi/src/main/java/cn/tedu/mall/order/mapper/OmsCartMapper.java
@@ -0,0 +1,21 @@
+package cn.tedu.mall.order.mapper;
+
+import cn.tedu.mall.pojo.order.model.OmsCart;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OmsCartMapper {
+
+    // 判断当前用户的购物车中是否已经包含指定商品的sku
+    OmsCart selectExistsCart(@Param("userId") Long userId,
+                             @Param("skuId") Long skuId);
+
+    // 新增sku信息到购物车
+    int saveCart(OmsCart omsCart);
+
+    // 修改购物车中sku的数量
+    int updateQuantityById(OmsCart omsCart);
+
+
+}
