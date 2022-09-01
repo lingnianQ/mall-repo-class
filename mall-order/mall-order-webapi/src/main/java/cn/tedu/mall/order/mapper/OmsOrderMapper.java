d5e42442254226d8bf1c9be4254526a7e9c24444
liucangsong <liucangsong@gmail.com>
2022-09-01 15:55:57 +0800

    查询订单

--- mall-order/mall-order-webapi/src/main/java/cn/tedu/mall/order/mapper/OmsOrderMapper.java
+++ mall-order/mall-order-webapi/src/main/java/cn/tedu/mall/order/mapper/OmsOrderMapper.java
@@ -1,12 +1,21 @@
 package cn.tedu.mall.order.mapper;
 
+import cn.tedu.mall.pojo.order.dto.OrderListTimeDTO;
 import cn.tedu.mall.pojo.order.model.OmsOrder;
+import cn.tedu.mall.pojo.order.vo.OrderListVO;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface OmsOrderMapper {
 
     // 新增订单的mapper方法
     int insertOrder(OmsOrder order);
+
+    // 查询当前用户指定时间范围的所有订单
+    List<OrderListVO> selectOrdersBetweenTimes(
+                            OrderListTimeDTO orderListTimeDTO);
+
 
 }
