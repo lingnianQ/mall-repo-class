6282f20dda7fd964510904083ce52530160f585a
liucangsong <liucangsong@gmail.com>
2022-09-02 09:46:03 +0800

    查询订单

--- mall-order/mall-order-webapi/src/main/java/cn/tedu/mall/order/mapper/OmsOrderMapper.java
+++ mall-order/mall-order-webapi/src/main/java/cn/tedu/mall/order/mapper/OmsOrderMapper.java
@@ -17,5 +17,12 @@ public interface OmsOrderMapper {
     List<OrderListVO> selectOrdersBetweenTimes(
                             OrderListTimeDTO orderListTimeDTO);
 
+    // 动态修改订单的sql,参数是omsOrder实体类对象
+    // 对象中必须有id,id不可修改,其它属性不为空就修改其它属性值
+    int updateOrderById(OmsOrder order);
+
+
+
+
 
 }
