20dd6a25360be7a9bcf7094971f59a75925e5fc2
liucangsong <liucangsong@gmail.com>
2022-09-08 09:55:30 +0800

    缓存预热

---
+++ mall-seckill/mall-seckill-webapi/src/main/java/cn/tedu/mall/seckill/exception/SeckillFallBack.java
@@ -0,0 +1,24 @@
+package cn.tedu.mall.seckill.exception;
+
+import cn.tedu.mall.common.restful.JsonResult;
+import cn.tedu.mall.common.restful.ResponseCode;
+import cn.tedu.mall.pojo.seckill.dto.SeckillOrderAddDTO;
+import com.baomidou.mybatisplus.extension.api.R;
+import lombok.extern.slf4j.Slf4j;
+
+// 秒杀业务的降级处理类
+@Slf4j
+public class SeckillFallBack {
+
+    // 返回值和控制器方法一致
+    // 参数也是包含控制器的所有方法,可以不写其它参数,也可以添加一个Throwable类型的参数
+    // Throwable类型的参数就是触发降级的异常对象
+    public static JsonResult seckillFallback(String randCode,
+                                             SeckillOrderAddDTO seckillOrderAddDTO,
+                                             Throwable throwable){
+        log.error("一个请求被降级了");
+        return JsonResult.failed(ResponseCode.INTERNAL_SERVER_ERROR,
+                throwable.getMessage());
+    }
+
+}
