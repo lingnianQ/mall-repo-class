e67f9f6400770dccb3539446189b04c17dace347
jason <jason@gmail.com>
2022-09-30 11:50:02 +0800

    分类功能

--- mall-front/mall-front-webapi/src/main/java/cn/tedu/mall/front/controller/FrontSpuController.java
+++ mall-front/mall-front-webapi/src/main/java/cn/tedu/mall/front/controller/FrontSpuController.java
@@ -3,7 +3,9 @@ package cn.tedu.mall.front.controller;
 import cn.tedu.mall.common.restful.JsonPage;
 import cn.tedu.mall.common.restful.JsonResult;
 import cn.tedu.mall.front.service.IFrontProductService;
+import cn.tedu.mall.pojo.product.vo.AttributeStandardVO;
 import cn.tedu.mall.pojo.product.vo.SpuListItemVO;
+import cn.tedu.mall.pojo.product.vo.SpuStandardVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
@@ -13,6 +15,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/front/spu")
@@ -37,5 +41,30 @@ public class FrontSpuController {
         return JsonResult.ok(jsonPage);
 
     }
+
+    // 根据spuId查询spu信息
+    // localhost:10004/front/spu/1
+    @GetMapping("/{spuId}")
+    @ApiOperation("根据spuId查询spu信息")
+    @ApiImplicitParam(value = "spuId",name="spuId",example = "1")
+    public JsonResult<SpuStandardVO> getFrontSpuById(@PathVariable Long spuId){
+        SpuStandardVO spuStandardVO=frontProductService.getFrontSpuById(spuId);
+        return JsonResult.ok(spuStandardVO);
+    }
+
+    // 根据spuId查询所有参数
+    @GetMapping("/template/{id}")
+    @ApiOperation("根据spuId查询所有参数")
+    @ApiImplicitParam(value = "spuId",name="id",example = "1")
+    public JsonResult<List<AttributeStandardVO>> getAttributeBySpuId(
+                        @PathVariable Long id){
+        List<AttributeStandardVO> list=frontProductService
+                                    .getSpuAttributesBySpuId(id);
+        return JsonResult.ok(list);
+    }
+
+
+
+
 
 }
