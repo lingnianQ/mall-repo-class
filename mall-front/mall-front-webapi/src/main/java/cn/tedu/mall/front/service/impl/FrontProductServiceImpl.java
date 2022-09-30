28088fdefe4174f6362ae0aca5514e72b7c00e42
jason <jason@gmail.com>
2022-09-30 11:32:32 +0800

    分类功能

--- mall-front/mall-front-webapi/src/main/java/cn/tedu/mall/front/service/impl/FrontProductServiceImpl.java
+++ mall-front/mall-front-webapi/src/main/java/cn/tedu/mall/front/service/impl/FrontProductServiceImpl.java
@@ -3,6 +3,8 @@ package cn.tedu.mall.front.service.impl;
 import cn.tedu.mall.common.restful.JsonPage;
 import cn.tedu.mall.front.service.IFrontProductService;
 import cn.tedu.mall.pojo.product.vo.*;
+import cn.tedu.mall.product.service.front.IForFrontAttributeService;
+import cn.tedu.mall.product.service.front.IForFrontSkuService;
 import cn.tedu.mall.product.service.front.IForFrontSpuService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.annotation.DubboReference;
@@ -16,6 +18,14 @@ public class FrontProductServiceImpl implements IFrontProductService {
 
     @DubboReference
     private IForFrontSpuService dubboSpuService;
+    // 消费根据spuId查询sku列表的功能
+    @DubboReference
+    private IForFrontSkuService dubboSkuService;
+    // 消费根据spuId查询当前spu对应的所有参数\规格的功能
+    @DubboReference
+    private IForFrontAttributeService dubboAttributeService;
+
+
 
     @Override
     public JsonPage<SpuListItemVO> listSpuByCategoryId(Long categoryId, Integer page, Integer pageSize) {
@@ -26,23 +36,30 @@ public class FrontProductServiceImpl implements IFrontProductService {
         return list;
     }
 
+    // 根据spuId查询spu信息
     @Override
     public SpuStandardVO getFrontSpuById(Long id) {
-        return null;
+        SpuStandardVO spuStandardVO=dubboSpuService.getSpuById(id);
+        return spuStandardVO;
     }
-
+    // 根据spuId查询对应的sku列表
     @Override
     public List<SkuStandardVO> getFrontSkusBySpuId(Long spuId) {
-        return null;
+        List<SkuStandardVO> list=dubboSkuService.getSkusBySpuId(spuId);
+        return list;
     }
-
+    // 根据spuId查询spuDetail信息
     @Override
     public SpuDetailStandardVO getSpuDetail(Long spuId) {
-        return null;
+        SpuDetailStandardVO spuDetailStandardVO=
+                dubboSpuService.getSpuDetailById(spuId);
+        return spuDetailStandardVO;
     }
-
+    // 根据spuId查询对应的所有属性列表
     @Override
     public List<AttributeStandardVO> getSpuAttributesBySpuId(Long spuId) {
-        return null;
+        List<AttributeStandardVO> list=dubboAttributeService
+                .getSpuAttributesBySpuId(spuId);
+        return list;
     }
 }
