3bbb7d0f7f11ecebdf877297ae50080697eb89a9
jason <jason@gmail.com>
2022-10-08 09:56:00 +0800

    分类功能

--- mall-order/mall-order-webapi/src/main/java/cn/tedu/mall/order/service/impl/OmsCartServiceImpl.java
+++ mall-order/mall-order-webapi/src/main/java/cn/tedu/mall/order/service/impl/OmsCartServiceImpl.java
@@ -4,12 +4,15 @@ import cn.tedu.mall.common.exception.CoolSharkServiceException;
 import cn.tedu.mall.common.pojo.domain.CsmallAuthenticationInfo;
 import cn.tedu.mall.common.restful.JsonPage;
 import cn.tedu.mall.common.restful.ResponseCode;
+import cn.tedu.mall.order.mapper.OmsCartMapper;
 import cn.tedu.mall.order.service.IOmsCartService;
 import cn.tedu.mall.pojo.order.dto.CartAddDTO;
 import cn.tedu.mall.pojo.order.dto.CartUpdateDTO;
 import cn.tedu.mall.pojo.order.model.OmsCart;
 import cn.tedu.mall.pojo.order.vo.CartStandardVO;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
@@ -17,9 +20,37 @@ import org.springframework.stereotype.Service;
 @Service
 @Slf4j
 public class OmsCartServiceImpl implements IOmsCartService {
+
+    // 装配Mapper操作数据
+    @Autowired
+    private OmsCartMapper omsCartMapper;
+
     @Override
     public void addCart(CartAddDTO cartDTO) {
-
+        // 在查询购物车中是否有商品之前,必须先明确用户身份,也就是用户ID
+        Long userId=getUserId();
+        // 根据用户ID和skuId检查当前用户购物车中是否已经存在该商品
+        OmsCart omsCart=omsCartMapper.selectExistsCart(userId,cartDTO.getSkuId());
+        // 判断omsCart是不是null
+        if(omsCart==null){
+            // 如果omsCart为null,表示当前用户没有添加过这个商品,所以执行新增操作
+            // 因为新增操作方法的参数是一个OmsCart类型的对象,所以我们要先实例化出来
+            OmsCart newCart=new OmsCart();
+            // 将参数CartAddDTO对象的同名属性赋值给newCart
+            BeanUtils.copyProperties(cartDTO,newCart);
+            // CartAddDTO没有userId属性,需要单独赋值
+            newCart.setUserId(userId);
+            // 执行新增
+            omsCartMapper.saveCart(newCart);
+        }else{
+            // 如果omsCart不是null,表示当前用户之前已经将该sku添加到购物车中了
+            // 这种情况下,我们需要做的就是修改购物车中该sku的数量即可
+            // 是要在原有的商品数量基础上,再加本次新增商品的数量
+            // mapper中直接将值赋给数据库,所有在java代码里要将这个数量计算完成
+            omsCart.setQuantity(omsCart.getQuantity()+cartDTO.getQuantity());
+            // 然后调用修改购物车数量的方法
+            omsCartMapper.updateQuantityById(omsCart);
+        }
     }
 
     @Override
