77662bf6e99a75fbec205eb10ddc1075913ac71b
jtzhanghl <jtzhanghl@tedu.cn>
2022-10-10 16:20:47 +0800

    执行ES查询功能

--- mall-search/mall-search-webapi/src/main/java/cn/tedu/mall/search/repository/SpuForElasticRepository.java
+++ mall-search/mall-search-webapi/src/main/java/cn/tedu/mall/search/repository/SpuForElasticRepository.java
@@ -1,6 +1,7 @@
 package cn.tedu.mall.search.repository;
 
 import cn.tedu.mall.pojo.search.entity.SpuForElastic;
+import org.springframework.data.elasticsearch.annotations.Query;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,6 +13,20 @@ public interface SpuForElasticRepository extends
 
     // 查询title字段中包含指定关键字的spu数据
     Iterable<SpuForElastic> querySpuForElasticsByTitleMatches(String title);
+
+    @Query("{\n" +
+            "    \"bool\": {\n" +
+            "      \"should\": [\n" +
+            "        { \"match\": { \"name\": \"?0\"}},\n" +
+            "        { \"match\": { \"title\": \"?0\"}},\n" +
+            "        { \"match\": { \"description\": \"?0\"}},\n" +
+            "        { \"match\": { \"category_name\": \"?0\"}}\n" +
+            "        ]\n" +
+            "     }\n" +
+            "}")
+    // 上面指定了查询语句,这个方法的方法名就随意定义了
+    Iterable<SpuForElastic> querySearch(String keyword);
+
 
 
 }
