d56c8131a418c4589131d396b1e98d76d6224cb5
jason <jason@gmail.com>
2022-10-12 09:44:18 +0800

    分类功能

---
+++ mall-seckill/mall-seckill-webapi/src/main/java/cn/tedu/mall/seckill/mapper/SeckillSkuMapper.java
@@ -0,0 +1,14 @@
+package cn.tedu.mall.seckill.mapper;
+
+import cn.tedu.mall.pojo.seckill.model.SeckillSku;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface SeckillSkuMapper {
+
+    // 根据spuId查询sku列表
+    List<SeckillSku> findSeckillSkusBySpuId(Long spuId);
+
+}
