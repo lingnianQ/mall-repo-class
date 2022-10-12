ed7701331bfc01548ac4b49102e193b92723339f
jason <jason@gmail.com>
2022-10-12 11:29:26 +0800

    秒杀准备

--- mall-seckill/mall-seckill-webapi/src/main/java/cn/tedu/mall/seckill/timer/job/SeckillInitialJob.java
+++ mall-seckill/mall-seckill-webapi/src/main/java/cn/tedu/mall/seckill/timer/job/SeckillInitialJob.java
@@ -9,6 +9,7 @@ import org.quartz.JobExecutionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 @Slf4j
 public class SeckillInitialJob implements Job {
@@ -22,10 +23,26 @@ public class SeckillInitialJob implements Job {
     // 操作Redis的对象
     @Autowired
     private RedisTemplate redisTemplate;
-
+    /*
+    RedisTemplate对象在保存数据到Redis时,会先将当前数据序列化后保存
+    这样做的好处是序列化后的数据保存到Redis读写效率更高,缺点是不能在Redis中修改数据
+    我们现在要预热的是秒杀sku库存数,这个库存数如果也用上面的redisTemplate保存的方式
+    就容易在高并发情况下由于线程安全问题导致"超卖"
+    解决方案就是我们需要创建一个能够直接在Redis中修改数据的对象,来避免超卖的发生
+    SpringDataRedis提供了一个可以直接在Redis中操作数值的对象:StringRedisTemplate
+    使用StringRedisTemplate向Redis保存数据,默认都会以字符串的方式保存(不序列化了)
+    又因为Redis支持直接将字符串类型的数值进行修改,所以可以将库存数保存并直接修改
+    这样就不需要写java代码读取和修改了,直接使用Redis内部的功能修改库存数
+    最后结合Redis天生单线程的特性,避免线程安全问题方式超卖
+     */
+    @Autowired
+    private StringRedisTemplate stringRedisTemplatel;
 
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+
+
+
 
     }
 }
