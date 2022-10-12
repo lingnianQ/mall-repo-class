3a6375ce15722461fc31d6b14fa333ebc8d043f6
jason <jason@gmail.com>
2022-10-12 09:57:00 +0800

    秒杀准备

--- mall-seckill/mall-seckill-webapi/src/main/java/cn/tedu/mall/seckill/mapper/SeckillSpuMapper.java
+++ mall-seckill/mall-seckill-webapi/src/main/java/cn/tedu/mall/seckill/mapper/SeckillSpuMapper.java
@@ -3,12 +3,17 @@ package cn.tedu.mall.seckill.mapper;
 import cn.tedu.mall.pojo.seckill.model.SeckillSpu;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface SeckillSpuMapper {
     // 查询秒杀商品列表的方法
     List<SeckillSpu> findSeckillSpus();
+
+    // 根据指定时间,查询正在进行秒杀的商品
+    List<SeckillSpu> findSeckillSpusByTime(LocalDateTime time);
+
 }
 
 
