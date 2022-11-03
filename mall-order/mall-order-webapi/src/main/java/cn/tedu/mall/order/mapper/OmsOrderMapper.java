e70827d124d443b5b1c083ff2c88b7f5ed1a0ea8
lingnian <sytsnb@gmail.com>
2022-11-04 01:03:23 +0800

    查询订单列表

--- mall-order/mall-order-webapi/src/main/java/cn/tedu/mall/order/mapper/OmsOrderMapper.java
+++ mall-order/mall-order-webapi/src/main/java/cn/tedu/mall/order/mapper/OmsOrderMapper.java
@@ -1,7 +1,11 @@
 package cn.tedu.mall.order.mapper;
 
+import cn.tedu.mall.pojo.order.dto.OrderListTimeDTO;
 import cn.tedu.mall.pojo.order.model.OmsOrder;
+import cn.tedu.mall.pojo.order.vo.OrderListVO;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * @author sytsn
@@ -16,5 +20,13 @@ public interface OmsOrderMapper {
      * @return
      */
     int insertOrder(OmsOrder omsOrder);
+
+    /**
+     * 查询当前用户指定时间范围内的所有订单信息(包含订单项)
+     *
+     * @param orderListTimeDTO
+     * @return
+     */
+    List<OrderListVO> selectOrdersBetweenTimes(OrderListTimeDTO orderListTimeDTO);
 
 }
